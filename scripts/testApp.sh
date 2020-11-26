b7cbc69c12566edb20c23165c5c1edc7d6100791
Austin Seto <austin.seto@ibm.com>
2020-11-26 14:53:32 -0500

    Changes for daily builds

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -euxo pipefail
 
-##############################################################################
-##
-##  Travis CI test script
-##
-##############################################################################
+# Set up and start Minikube
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo ln -s $(pwd)/kubectl /usr/local/bin/kubectl
+wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
+chmod +x minikube
+
+sudo apt-get update -y
+sudo apt-get install -y conntrack
+
+run: sudo minikube start --vm-driver=none --bootstrapper=kubeadm
 
 # TEST 1: Building and running the application
 
