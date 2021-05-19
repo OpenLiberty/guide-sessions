deac81d4e9400a163269483a6b76491ebdee5f28
Austin Seto <austin.seto@ibm.com>
2021-05-19 14:45:19 -0400

    Minor file updates

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -15,8 +15,6 @@ mvn -Dhttp.keepAlive=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package liberty:create liberty:install-feature liberty:deploy
 
-mvn package
-
 ## Run the tests
 # These commands are separated because if one of the commands fail, the test script will fail and exit. 
 # e.g if liberty:start fails, then there is no need to run the failsafe commands. 
