c89edcdf547655b3f24507843d5f987f55949610
Gilbert Kwan <gkwan@ca.ibm.com>
2022-03-15 12:07:51 -0400

    Update dockerImageTest.sh

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -10,7 +10,8 @@ done
 
 echo "Testing latest OpenLiberty Docker image"
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" pom.xml
+#sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" pom.xml
+sed -i "\#<liberty.runtime.version>22.0.0.2</liberty.runtime.version>#c<install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" pom.xml
 cat pom.xml
 
 sed -i "s;FROM openliberty/open-liberty:full-java11-openj9-ubi;FROM openliberty/daily:latest;g" Dockerfile
