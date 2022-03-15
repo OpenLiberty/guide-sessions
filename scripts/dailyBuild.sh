3a921f2486b952b214771a4ed0a5381f98e8de2d
Gilbert Kwan <gkwan@ca.ibm.com>
2022-03-15 17:26:50 -0400

    Use OL 22.0.0.2 (#136)
    * Update pom.xml
    
    * Update pom.xml
    
    * Update dailyBuild.sh
    
    * Update dockerImageTest.sh
    
    * Update dockerImageTest.sh
    
    * Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -13,10 +13,11 @@ done
 echo "Testing daily build image"
 
 #sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" pom.xml
-sed -i "\#<liberty.runtime.version>22.0.0.2</liberty.runtime.version>#c<install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install>" pom.xml
+sed -i "\#<version>22.0.0.2</version>#c<runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl>" pom.xml
 cat pom.xml
 
-sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
+#sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
+sed -i "s;FROM icr.io/appcafe/open-liberty:22.0.0.2-full-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
 cat Dockerfile
 
 sudo ../scripts/startMinikube.sh
