18072e879aaffd52e998a3884d72393293e498e5
Gilbert Kwan <gkwan@ca.ibm.com>
2023-07-11 10:56:25 -0400

    Skip features.sh in docker test scripts

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -16,6 +16,7 @@ sed -i "\#</copyDependencies>#a<install><runtimeUrl>https://public.dhe.ibm.com/i
 cat pom.xml
 
 sed -i "s;FROM icr.io/appcafe/open-liberty:kernel-slim-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" Dockerfile
+sed -i "s;RUN features.sh;#RUN features.sh;g" Dockerfile
 cat Dockerfile
 
 sudo -u runner ../scripts/testApp.sh
